29ecf3c3f186150f5a36f31430c9f45344ba144a
Matheus Frauches <frauches.27@gmail.com>
2020-06-03 00:36:45 -0300

    Create API routes returning Items and Points data from database

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,13 @@
-import express from 'express';
+import express from "express";
+import path from "path";
+import routes from "./routes";
+import cors from 'cors';
 
 const app = express();
+app.use(cors());
+app.use(express.json());
+app.use(routes);
 
-app.get('/users', (request, response) => {
-  response.send("Hell Low!");
-});
+app.use("/uploads", express.static(path.resolve(__dirname, "..", "uploads")));
 
 app.listen(3333);

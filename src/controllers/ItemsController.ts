8064851bad65392df4b3bab2701f11e7296599c7
Matheus Frauches <frauches.27@gmail.com>
2020-06-04 23:23:54 -0300

    Add Item attribute image_url_mobile on getItem

--- src/controllers/ItemsController.ts
+++ src/controllers/ItemsController.ts
@@ -10,6 +10,7 @@ class ItemsController {
         id: item.id,
         title: item.title,
         image_url: `http://localhost:3333/uploads/${item.image}`,
+        image_url_mobile: `http://192.168.0.47:3333/uploads/${item.image}`,
       };
     });
   
